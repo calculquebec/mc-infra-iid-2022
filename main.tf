592bd9bb5baeab2c287f945431e993784db0f385
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-04-27 11:09:17 -0400

    Fix ref for dns

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ output "public_ip" {
 
 ## Uncomment to register your domain name with CloudFlare
 module "dns" {
-  source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare?ref=11.7"
+  source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare?ref=ext_data"
   email            = var.email
   name             = module.openstack.cluster_name
   domain           = module.openstack.domain
