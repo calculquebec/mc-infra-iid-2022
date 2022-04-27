26ec791fc4eac063c8aa9cac9c3b02869e46562d
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-04-27 09:30:16 -0400

    Add public key for Felix

--- main.tf
+++ main.tf
@@ -28,7 +28,9 @@ module "openstack" {
     }
   }
 
-  public_keys = [file("~/.ssh/id_rsa.pub")]
+  public_keys = [
+"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCy6vaeMkT+8lNOpvG+scN1iiRby35dGLyh1s3NOzufzqjAVvZL6t/vd7CN/hbTg+UUCc3V1hJOxGFGD3lrbXho1R5S4pZZ+Hge3icJm4BAPX6O9+H7aMWh/DE/vxHhyyC5QX8vg3AqUtCjihT03xftCC1XGzcP5lvs/bWgn1RS3pXY85UGY80NsRiGK08HpRER7ifNbo9zipIO4eQpRvTq6Ent5mqmeULMRX99SAbgUciaz5mUcULw5OimNFSjakdDRyF2xrdVsX6mwHPUT3UIXAxxS5GGTWIIH+jG0L0rqnjtnRwj8Imw+zOtE5itMg4kCNO+q5KJBH1jU6g57JFiEoxviHQH1ATlsw8e6gI2nhkfi10NgIzQ/yLxsK3IS4XwqMqDRInf6eGrf1CvbxYjOvpm7pOvLbvBdSrH+Ynx5X743oNIAmwjSoJ+ABa1RQp0XNrDfQXYmRrCA3sKgh/bOge+B8HDVEkCdSv+bAp/4rfGOKy4eOPsuzvrxnS8OWNet0+bAUV1Ta0VigHlFgEvQtS4/jrOO3It0ZaYMIZFAWqsjXimJ5LiT1TAgOCUO9950ZUA8BWQuSO+/EzREPCThRvFPKy82HAPOY8fo3jrEekuJdrjF8IDHxYL51YR+WPupY52HCHZkg4iDxKXnkrLS3K5/AIa5/+/CnHWL5WGHw== felix@armor.local",
+]
 
   nb_users = 100
   guest_passwd = var.password
