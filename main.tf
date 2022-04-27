751be76ba8e96850923e63f9bf84e48b2f550ab3
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-04-27 11:15:55 -0400

    Revert ref for dns

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ output "public_ip" {
 
 ## Uncomment to register your domain name with CloudFlare
 module "dns" {
-  source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare?ref=ext_data"
+  source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare?ref=11.7"
   email            = var.email
   name             = module.openstack.cluster_name
   domain           = module.openstack.domain
